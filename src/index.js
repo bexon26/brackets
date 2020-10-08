5d432813c47cc8ee1849add525a0471c46dcaf3f
bexon26 <47786103+bexon26@users.noreply.github.com>
2020-10-09 01:08:33 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  return true
+  while (str.length > 2) {
+    const conLength = str.length
+    let prevLength
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i].join(''), '')
+      prevLength = str.length
+    }
+    if (prevLength === conLength) return false
+  }
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (str === bracketsConfig[i].join('') || !str) return true
+  }
+  return false
 }
